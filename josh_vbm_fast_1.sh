c1a9682c255c003c7b159a3360c57f34094f4212
Josh Gilberstadt <joshgilberstadt@Joshs-MacBook-Pro.local>
2022-01-19 01:29:13 -0600

    Segment brain

---
+++ josh_vbm_fast_1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+######## Job Name: Sample_Job ########
+#SBATCH -J Sample_Job
+######## Job Output File: Sample_job.oJOBID ########
+#SBATCH -o /home/gilberstadt.j/Upload_from_Local/josh_vbm_fast_1_jobs/Sample_job.o%j
+######## Job Error File: Sample_job.eJOBID ########
+#SBATCH -e /home/gilberstadt.j/Upload_from_Local/josh_vbm_fast_1_jobs/Sample_job.e%j
+######## Email
+#SBATCH --mail-user=gilberstadt.j@wustl.edu
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+######## Number of nodes: 1 ########
+#SBATCH -N 1
+######## Number of tasks: 1 ########
+#SBATCH -n 1
+######## Memory per node: 200 MB ########
+#SBATCH --mem 5G
+######## Walltime: 30 minutes ########
+#SBATCH -t 02:00:00
+
+module load fsl
+module list
+
+fullpath=$1  # the full path to the image
+dirpath=$(dirname ${fullpath}) # gives you the path to the directory where the image lives
+fname=$(basename ${fullpath}) # gives you the name of the image file
+fbase=$(echo ${fname} | rev | cut -f3- -d '.' | rev) # gives you the name of the image file without the postfix (i.e., .nii.gz)
+
+echo "fast -o ${dirpath}/FAST/${fbase}_brain ${dirpath}/BET/${fbase}_brain.nii.gz"
+echo "fast starts"
+if [ ! -d "${dirpath}/FAST" ]
+then
+        mkdir ${dirpath}/FAST
+fi
+fast -o ${dirpath}/FAST/${fbase}_brain ${dirpath}/BET/${fbase}_brain.nii.gz
+success=$(echo $?)
+if (($success==0))
+then
+        echo "success"
+fi
+echo "fast ends"
