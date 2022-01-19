85fbd29cb27727e82e011953b52a80625e6ac806
Josh Gilberstadt <joshgilberstadt@Joshs-MacBook-Pro.local>
2022-01-19 01:56:30 -0600

    Wrapper for josh_vbm_flirt_2.sh

---
+++ wrapper_flirt_2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+######## Job Name: Sample_Job ########
+#SBATCH -J Sample_Job
+######## Job Output File: Sample_job.oJOBID ########
+#SBATCH -o /home/gilberstadt.j/Upload_from_Local/wrapper_flirt_2_jobs/Sample_job.o%j
+######## Job Error File: Sample_job.eJOBID ########
+#SBATCH -e /home/gilberstadt.j/Upload_from_Local/wrapper_flirt_2_jobs/Sample_job.e%j
+######## Email
+#SBATCH --mail-user=gilberstadt.j@wustl.edu
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+######## Number of nodes: 1 ########
+#SBATCH -N 1
+######## Number of tasks: 1 ########
+#SBATCH -n 1
+######## Memory per node: 200 MB ########
+#SBATCH --mem 5G
+######## Walltime: 30 minutes ########
+#SBATCH -t 02:00:00
+
+job_number=1
+total_jobs=$(wc -l $1 | cut --complement -f2 -d ' ')
+for fullpath in $(cat $1);
+do
+	sbatch /home/gilberstadt.j/josh_vbm_flirt_2.sh $fullpath
+	echo "Submitted job number $job_number of $total_jobs"
+        ((job_number+=1))
+done
