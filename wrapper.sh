d3c5f42847b96c4f3b62bd57064087ea6f2fa9d4
Josh Gilberstadt <joshgilberstadt@Joshs-MacBook-Pro.local>
2022-01-19 01:52:03 -0600

    Wrapper for josh_vbm_warp.sh

---
+++ wrapper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+######## Job Name: Sample_Job ########
+#SBATCH -J Sample_Job
+######## Job Output File: Sample_job.oJOBID ########
+#SBATCH -o /home/gilberstadt.j/Upload_from_Local/wrapper_jobs/Sample_job.o%j
+######## Job Error File: Sample_job.eJOBID ########
+#SBATCH -e /home/gilberstadt.j/Upload_from_Local/wrapper_jobs/Sample_job.e%j
+######## Email
+#SBATCH --mail-user=gilberstadt.j@wustl.edu
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+######## Number of nodes: 1 ########
+#SBATCH -N 1
+######## Number of tasks: 1 ########
+#SBATCH -n 1
+######## Memory per node: 200 MB ########
+#SBATCH --mem 5G
+######## Walltime: 30 minutes ########
+#SBATCH -t 02:00:00
+
+for i in "$@";
+do
+	for j in $(cat $i | rev | cut --complement -f2- -d '/' | rev);
+	do
+		sbatch /home/gilberstadt.j/josh_vbm_warp.sh $j
+	done
+done
